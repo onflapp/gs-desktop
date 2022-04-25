a11b3a56f09b5e5290700ad9f65cd0768f597033
onflapp <onflapp@gmail.com>
2022-04-25 14:57:07 +0200

    --

--- system/06-build_gnustep-back.sh
+++ system/06-build_gnustep-back.sh
@@ -5,11 +5,13 @@
 cd ../../gnustep-back || exit 1
 
 make clean
+./configure --enable-graphics=art --with-name=art
+make fonts=no || exit 1
 
-#./configure --enable-graphics=art --with-name=art
-#make fonts=no || exit 1
+sudo -E make install
 
+make clean
 ./configure --enable-graphics=cairo --with-name=cairo
+make fonts=no || exit 1
 
-make || exit 1
 sudo -E make install
