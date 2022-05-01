810e4f7f85f743748510b650bfd0503508def68b
onflapp <onflapp@gmail.com>
2022-05-01 17:14:51 +0200

    --

--- Applications/fetch_applications.sh
+++ Applications/fetch_applications.sh
@@ -3,9 +3,43 @@
 set -e
 
 cd ../../
+D=`pwd`
 
-git clone https://github.com/onflapp/gs-terminal.git
-git clone https://github.com/onflapp/gs-webbrowser.git
-git clone https://github.com/onflapp/gs-textedit.git
-git clone https://github.com/gnustep/apps-gorm.git
-git clone https://github.com/gnustep/apps-projectcenter.git
+if [ -d gs-terminal ];then
+  cd ./gs-terminal
+  git pull
+else
+  git clone https://github.com/onflapp/gs-terminal.git
+fi
+
+cd "$D"
+if [ -d gs-webbrowser ];then
+  cd ./gs-webbrowser
+  git pull
+else
+  git clone https://github.com/onflapp/gs-webbrowser.git
+fi
+
+cd "$D"
+if [ -d gs-textedit ];then
+  cd ./gs-textedit
+  git pull
+else
+  git clone https://github.com/onflapp/gs-textedit.git
+fi
+
+cd "$D"
+if [ -d apps-gorm ];then
+  cd ./apps-gorm
+  git pull
+else
+  git clone https://github.com/gnustep/apps-gorm.git
+fi
+
+cd "$D"
+if [ -d apps-projectcenter ];then
+  cd ./apps-projectcenter
+  git pull
+else
+  git clone https://github.com/gnustep/apps-projectcenter.git
+fi
