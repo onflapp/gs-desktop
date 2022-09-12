ee5ec0bdd63b93b74d1c5114a7806a9c48d40cb5
= <onflapp@gmail.com>
2022-09-12 17:56:01 +0200

    add DocumentViever.app

---
+++ Applications/DocumentViewer/AppController.m
@@ -0,0 +1,71 @@
+/* 
+   Project: DocumentViewer
+
+   Author: Ondrej Florian,,,
+
+   Created: 2022-09-12 13:07:11 +0200 by oflorian
+   
+   Application Controller
+*/
+
+#import "AppController.h"
+
+@implementation AppController
+
++ (void) initialize
+{
+  NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
+
+  /*
+   * Register your app's defaults here by adding objects to the
+   * dictionary, eg
+   *
+   * [defaults setObject:anObject forKey:keyForThatObject];
+   *
+   */
+  
+  [[NSUserDefaults standardUserDefaults] registerDefaults: defaults];
+  [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (id) init
+{
+  if ((self = [super init]))
+    {
+    }
+  return self;
+}
+
+- (void) dealloc
+{
+  [super dealloc];
+}
+
+- (void) awakeFromNib
+{
+}
+
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif
+{
+}
+
+- (BOOL) applicationShouldTerminate: (id)sender
+{
+  return YES;
+}
+
+- (void) applicationWillTerminate: (NSNotification *)aNotif
+{
+}
+
+- (BOOL) application: (NSApplication *)application
+            openFile: (NSString *)fileName
+{
+  return NO;
+}
+
+- (void) showPrefPanel: (id)sender
+{
+}
+
+@end
