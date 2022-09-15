f046744c5d57b36b6c754683dee31692d4accd9e
onflapp <onflapp@gmail.com>
2022-09-15 21:34:32 +0200

    --

--- Applications/02-build_tools.sh
+++ Applications/02-build_tools.sh
@@ -3,68 +3,27 @@ D=`pwd`
 
 . /Developer/Makefiles/GNUstep.sh
 
-cd ./Tools || exit 1
+function build_app {
+  cd "$D"
+  cd ./$1 || exit 1
 
-make clean
-make -j2 || exit 1
+  make clean
+  make -j2 || exit 1
 
-sudo -E make install
+  sudo -E make install
+}
 
-cd "$D"
-cd ./Addresses || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
-
-cd "$D"
-cd ./FontManager || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
-
-cd "$D"
-cd ./BatMon || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
-
-cd "$D"
-cd ./OpenUp || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
-
-cd "$D"
-cd ./ImageViewer || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
-
-cd "$D"
-cd ./DocumentViewer || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
-
-cd "$D"
-cd ./ScreenShot || exit 1
-
-make clean
-make -j2 || exit 1
-
-sudo -E make install
+build_app "Tools"
+build_app "Affiche"
+build_app "FontManager"
+build_app "Calculator"
+build_app "DefaultsManager"
+build_app "HelpViewer"
+build_app "BatMon"
+build_app "OpenUp"
+build_app "ImageViewer"
+build_app "DocumentViewer"
+build_app "ScreenShot"
 
 cd "$D"
 sudo cp -R ./WPrefs.app /Applications
