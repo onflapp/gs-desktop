a1ddfcafdf36b5f389ba12a1807c67fc054f372e
onflapp <onflapp@gmail.com>
2022-11-08 10:22:37 +0100

    --

--- Applications/Player/Resources/playerview/start_audio.sh
+++ Applications/Player/Resources/playerview/start_audio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 unset TERM
-vlc -q -I rc --rc-show-pos --no-playlist-autostart $@
+vlc -q -I rc --rc-show-pos --no-playlist-autostart "$1"
 #vlc -I rc --rc-fake-tty --rc-show-pos
 #vlc --width 100 --no-autoscale --drawable-xid 0x1a1c516
--- Applications/Player/Resources/playerview/start_video.sh
+++ Applications/Player/Resources/playerview/start_video.sh
@@ -2,5 +2,4 @@
 
 unset TERM
 XID="$1"
-shift
-vlc -q -I rc --rc-show-pos --no-playlist-autostart --drawable-xid "$XID" $@
+vlc -q -I rc --rc-show-pos --no-playlist-autostart --drawable-xid "$XID" "$2"
