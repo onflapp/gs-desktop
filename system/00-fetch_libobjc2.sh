93ce7177a9170426de9746970531769c7fea96a2
onflapp <onflapp@gmail.com>
2023-04-14 22:09:52 +0200

    --

--- system/00-fetch_libobjc2.sh
+++ system/00-fetch_libobjc2.sh
@@ -9,6 +9,15 @@ else
   git clone https://github.com/gnustep/libobjc2.git
   cd ./libobjc2
 
+  if [ `dpkg --print-architecture 2>/dev/null` = "armhf" ];then
+
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "this is ARM 32bit, we need to downgrade to version 1.9"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+    git checkout origin/1.9
+  fi
+
   git submodule init
   git submodule update
 fi
