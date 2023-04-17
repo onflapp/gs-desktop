7682cb29b74c481391dc5089ef7e51a35c5030f5
onflapp <onflapp@gmail.com>
2023-04-17 11:23:25 +0200

    fix build for ARM32

--- system/00-fetch_libobjc2.sh
+++ system/00-fetch_libobjc2.sh
@@ -2,19 +2,25 @@
 
 cd ../../
 
+if [ `dpkg --print-architecture 2>/dev/null` = "armhf" ];then
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "this is ARM 32bit, we need to downgrade to version 1.9"
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  BRANCH="origin/1.9"
+fi
+  
 if [ -d libobjc2 ];then
   cd ./libobjc2
-  git pull
+  if [ -n "$BRANCH" ];then
+    echo "...in branch $BRANCH, skip pull"
+  else
+    git pull
+  fi
 else
   git clone https://github.com/gnustep/libobjc2.git
   cd ./libobjc2
 
-  if [ `dpkg --print-architecture 2>/dev/null` = "armhf" ];then
-
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo "this is ARM 32bit, we need to downgrade to version 1.9"
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
+  if [ -n "$BRANCH" ];then
     git checkout origin/1.9
   fi
 
