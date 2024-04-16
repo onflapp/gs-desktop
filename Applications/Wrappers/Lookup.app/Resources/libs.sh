03ede944183d170b27ab0d4f40b6d01b3f4af509
onflapp <onflapp@gmail.com>
2024-04-16 18:41:50 +0200

    updated scripts

---
+++ Applications/Wrappers/Lookup.app/Resources/libs.sh
@@ -0,0 +1,18 @@
+urlencode() {
+  LC_ALL=C awk -- '
+    BEGIN {
+      for (i = 1; i <= 255; i++) hex[sprintf("%c", i)] = sprintf("%%%02X", i)
+    }
+    function urlencode(s,  c,i,r,l) {
+      l = length(s)
+      for (i = 1; i <= l; i++) {
+        c = substr(s, i, 1)
+        r = r "" (c ~ /^[-._~0-9a-zA-Z]$/ ? c : hex[c])
+      }
+      return r
+    }
+    BEGIN {
+      for (i = 1; i < ARGC; i++)
+        print urlencode(ARGV[i])
+    }' "$@"
+}
